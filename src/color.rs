66f809ff365a2c11b350f9d9d859dc12c7c3a4bc
Munechika Sumikawa <sumikawa@sumikawa.jp>
2026-02-04 13:04:11 +0900

    First step to draw line in 2D

---
+++ src/color.rs
@@ -0,0 +1,26 @@
+use std::fmt;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.r, self.g, self.b)
+    }
+}
+
+impl Default for Color {
+    fn default() -> Self {
+        Self::new(0, 0, 0)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use image::ImageBuffer;
+use mypolygon::color::Color;
+
+fn draw_line(
+    fb: &mut ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+    x0: i32,
+    y0: i32,
+    x1: i32,
+    y1: i32,
+    color: Color,
+) {
+    for x in x0..=x1 {
+        let t = (x - x0) as f64 / (x1 - x0) as f64;
+        let y = (y0 as f64 * (1.0 - t) + y1 as f64 * t) as i32;
+        fb.put_pixel(x as u32, y as u32, image::Rgb([color.r, color.g, color.b]));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 384;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+
+    let mut imgbuf = image::ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    draw_line(&mut imgbuf, 30, 30, 300, 150, Color::new(255, 255, 0));
+
+    imgbuf.save("output.png").unwrap();
 }
