3ec8a90df1c74af8882c9eae9d8e0ebcf22f6ad8
Munechika Sumikawa <sumikawa@sumikawa.jp>
2026-02-04 15:27:12 +0900

    Put aspect ration and image width on config file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use image::ImageBuffer;
 use mypolygon::color::Color;
+use mypolygon::config::Settings;
 
 fn draw_line(
     fb: &mut ImageBuffer<image::Rgb<u8>, Vec<u8>>,
@@ -15,28 +16,24 @@ fn draw_line(
     let mut y = y0;
 
     for x in x0..=x1 {
-        fb.put_pixel(
-            x as u32,
-            y as u32,
-            image::Rgb([color.r, color.g, color.b]),
-        );
+        fb.put_pixel(x as u32, y as u32, image::Rgb([color.r, color.g, color.b]));
 
         if d > 0 {
             d += 2 * (dy - dx);
-	    y += 1;
+            y += 1;
         } else {
             d += 2 * dy;
         }
-
     }
 }
 
 fn main() {
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u32 = 384;
-    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+    let settings = Settings::new();
+    let aspect_ratio = settings.aspect_ratio;
+    let image_width = settings.image_width;
+    let image_height = (image_width as f64 / aspect_ratio) as u32;
 
-    let mut imgbuf = image::ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
+    let mut imgbuf = image::ImageBuffer::new(image_width, image_height);
 
     draw_line(&mut imgbuf, 30, 30, 300, 150, Color::new(255, 255, 0));
 
